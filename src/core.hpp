85843438b3a3544bd9b385997f39fa071674336b
Yuuko <tilio.huart@epitech.eu>
2026-03-04 13:10:48 +0100

    feat: initialize simulation class

--- src/core.hpp
+++ src/core.hpp
@@ -2,13 +2,12 @@
 
 #include <entt/entity/registry.hpp>
 
+
 namespace core {
     class Simulation {
         public:
             void initializeCore();
         private:
-            void initializeCoreEntity();
-            void initializeCoreSystem();
             entt::registry _registry;
     };
 } // namespace core
