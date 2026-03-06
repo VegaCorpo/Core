6a03f6f443c0c7ebe63c43eb05ff41e3818b48c6
Lucas <lucas.ludovic@epitech.eu>
2026-03-06 09:56:13 +0100

    fix: restore prototype init

--- src/core.cpp
+++ src/core.cpp
@@ -27,6 +27,23 @@ core::SimulationState core::Simulation::initializeCore() noexcept
 
 core::SimulationState core::Simulation::_loadEngines() noexcept
 {
+    const auto earthEntity = this->_registry.create();
+    this->_registry.emplace<components::Mass>(earthEntity, components::Mass(EARTH_MANTISSA, EARTH_EXPONENT));
+    this->_registry.emplace<components::Acceleration>(earthEntity, components::Acceleration(0, 0));
+    this->_registry.emplace<components::Position>(earthEntity, components::Position(149597870.f, 0.f, 0.f));
+    this->_registry.emplace<components::Velocity>(earthEntity, components::Velocity(0.f, 0.f, 0.f));
+
+    const auto moonEntity = this->_registry.create();
+    this->_registry.emplace<components::Mass>(moonEntity, components::Mass(MOON_MANTISSA, MOON_EXPONENT));
+    this->_registry.emplace<components::Acceleration>(moonEntity, components::Acceleration(0, 0));
+    this->_registry.emplace<components::Position>(moonEntity, components::Position(149213470.f, 0.f, 0.f));
+    this->_registry.emplace<components::Velocity>(moonEntity, components::Velocity(0.f, 0.f, 0.f));
+
+    const auto sunEntity = this->_registry.create();
+    this->_registry.emplace<components::Mass>(sunEntity, components::Mass(SUN_MANTISSA, SUN_EXPONENT));
+    this->_registry.emplace<components::Acceleration>(sunEntity, components::Acceleration(0, 0));
+    this->_registry.emplace<components::Position>(sunEntity, components::Position(0.0f, 0.f, 0.f));
+    this->_registry.emplace<components::Velocity>(sunEntity, components::Velocity(0.f, 0.f, 0.f));
 
     this->_loader.load<void(void*, double)>("plugins/Physics/liborbital_physics", "physicsUpdate", "physicsUpdate");
     this->_loader.load<std::string()>("plugins/Physics/liborbital_physics", "getName", "getName");
