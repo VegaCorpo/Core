e579a3f0014b82f35e60b1667c6ed07866d78f20
Yuuko <tilio.huart@epitech.eu>
2026-03-10 10:37:36 +0100

    fix: check args

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <cstdlib>
 #include "src/core.hpp"
 
+int checkArg(int const argc, char const* argv[]) {
+    if (argc != 2) {
+        return EXIT_FAILURE;
+    }
+    if (argv == nullptr || argv[1] == nullptr) {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
 int main(int const argc, char const* argv[])
 {
     core::Simulation simulation;
 
+    if (checkArg(argc, argv) == EXIT_FAILURE)
+        return EXIT_FAILURE;
     simulation.initializeCore(argv[1]);
     simulation.launchSimulation();
     return EXIT_SUCCESS;
